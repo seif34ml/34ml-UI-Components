d5ed860c3ab930a069d83591bb6d96b0a419f3ed
seif1125 <seifammar1125@gmail.com>
2022-11-17 17:14:21 +0200

    Add Desktop menu options

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,180 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld class="bg-gray-900" msg="Welcome to Your Vue.js App" />
+    <MainHeader :HeaderOptions="HeaderOptions" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import MainHeader from "./components/header/MainHeader.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    MainHeader,
+  },
+  data() {
+    return {
+      HeaderOptions: {
+        logo: "logo.png",
+        menu: [
+          {
+            main_title: "magazine",
+            url: "read",
+            has_submenu: true,
+            mobile_submenu: true,
+            mobile_url: "read",
+            submenu: [
+              {
+                title: "pregnancy test",
+                url: "read/categories/pregnancy",
+                list: [
+                  { title: "Mums", url: "read/categories/pregnancy/mums" },
+                ],
+              },
+              {
+                title: "parent",
+                url: "read/categories/parent",
+                list: [{ title: "sub", url: "read/categories/parent/sub" }],
+              },
+              {
+                title: "test test",
+                url: "read/categories/test-test",
+                list: [
+                  {
+                    title: "Test subcategory",
+                    url: "read/categories/test-test/test-subcategory",
+                  },
+                ],
+              },
+              {
+                title: "Child",
+                url: "read/categories/child",
+                list: [
+                  {
+                    title: "Baby Development",
+                    url: "read/categories/child/baby-development",
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            main_title: "watch",
+            url: "watch",
+            has_submenu: true,
+            mobile_submenu: true,
+            mobile_url: "watch",
+            submenu: [
+              {
+                title: "Series",
+                url: "watch/series",
+                list: [
+                  {
+                    title: "why moms love murder1",
+                    url: "watch/series/why-moms-love-murder1",
+                  },
+                  {
+                    title: "why moms love murder2",
+                    url: "watch/series/why-moms-love-murder2",
+                  },
+                  {
+                    title: "donot start your own business: a series",
+                    url: "watch/series/donot-start-your-own-business-a-series",
+                  },
+                  { title: "rrrkl", url: "watch/series/rrrkl" },
+                  {
+                    title: "why moms love murder3",
+                    url: "watch/series/why-moms-love-murder3",
+                  },
+                  {
+                    title: "Success stories every mother should know",
+                    url: "watch/series/success-stories-every-mother-should-know",
+                  },
+                  {
+                    title: "Your guide to entrepreneurship as a mom",
+                    url: "watch/series/your-guide-to-entrepreneurship-as-a-mom",
+                  },
+                  {
+                    title: "workout, eat healthy, stay happy",
+                    url: "watch/series/workout-eat-healthy-stay-happy",
+                  },
+                  {
+                    title: "how to talk so teens can listen",
+                    url: "watch/series/how-to-talk-so-teens-can-listen",
+                  },
+                ],
+              },
+              {
+                title: "Videos",
+                url: "watch/videos",
+                list: [
+                  {
+                    title: "pregnancy test",
+                    url: "watch/categories/pregnancy",
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            main_title: "shop",
+            url: "products",
+            has_submenu: false,
+            mobile_submenu: false,
+            submenu: [{ title: "", url: "", list: [] }],
+          },
+          {
+            main_title: "get fit",
+            url: "fit-programs",
+            has_submenu: false,
+            mobile_submenu: false,
+            submenu: [{ title: "", url: "", list: [] }],
+          },
+          {
+            main_title: "card",
+            url: "card",
+            has_submenu: false,
+            mobile_submenu: false,
+            submenu: [{ title: "", url: "", list: [] }],
+          },
+          {
+            main_title: "events",
+            url: "events",
+            has_submenu: false,
+            mobile_submenu: false,
+            submenu: [{ title: "", url: "", list: [] }],
+          },
+          {
+            main_title: "about",
+            url: "about-us",
+            has_submenu: false,
+            mobile_submenu: true,
+            submenu: [
+              { title: "about us", url: "about-us", list: [] },
+              { title: "contact us", url: "contact-us", list: [] },
+              {
+                title: "advertise with us",
+                url: "advertise-with-us",
+                list: [],
+              },
+              { title: "frequently asked questions", url: "/faqs", list: [] },
+              {
+                title: "terms & conditions",
+                url: "pages/terms/content",
+                list: [],
+              },
+              {
+                title: "privacy policy",
+                url: "pages/privacy-policy/content",
+                list: [],
+              },
+            ],
+          },
+        ],
+      },
+    };
   },
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/components/header/MainHeader.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav>
+    <HeaderDesktop
+      class="hidden lg:flex"
+      :menu="HeaderOptions.menu"
+      :logo="HeaderOptions.logo"
+    />
+  </nav>
+</template>
+
+<script>
+import HeaderDesktop from "./HeaderDesktop.vue";
+export default {
+  name: "MainHeader",
+  components: { HeaderDesktop },
+  props: {
+    HeaderOptions: Object,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
